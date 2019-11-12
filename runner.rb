40fb84624827cacd4f723104685befcecf8f1bf1
Daniel Mulitauopele <daniel.mulitauopele@logrhythm.com>
2019-11-12 15:55:11 -0700

    Adjust mock dependencies

--- runner.rb
+++ runner.rb
@@ -2,4 +2,4 @@ require './dependency_finder'
 
 df = DependencyFinder.new
 
-df.write_to_excel("opportunity_field_names.txt", [1, 2, 3, 4, 5])
+df.write_to_excel("opportunity_field_names.txt", [0, 1, 4, 12])
